fb7e1bf250e2dc6920d7ef27ae581fa62c5c7ca6
AyaAlahdl <aya.mohammed.hashem@gmail.com>
2023-03-18 09:03:16 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,16 +14,16 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		for (j = 1 ; j < 10 ; j++)
 		{
-			for (k = 2; k < 10 ; k++)
+			for (k = 2 ; k < 10 ; k++)
 			{
 				if (i < j && j < k)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i + j + k != 21)
+					if (i + j + k != 24)
 					{
 						putchar(',');
 						putchar(' ');
