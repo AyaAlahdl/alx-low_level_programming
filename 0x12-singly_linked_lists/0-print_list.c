a71d2a2359ba32f97829571cdd31d16b053a6fae
AyaAlahdl <aya.mohammed.hashem@gmail.com>
2023-04-27 05:45:35 -0700

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,6 @@
 
 
 /**
- * _strlen - return the lenght of a string.
- * @s: The string whose length to check
- *
- * Return: integer.
- */
-
-int _strlen(char *s)
-{
-
-	int i = 0;
-
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
@@ -34,9 +14,14 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		n++;
+		h = h->next;
 	}
 
 	return (n);
