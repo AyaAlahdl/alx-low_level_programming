c4b02c7667e69a8fc3ceb135d8559b6fab45e60e
AyaAlahdl <aya.mohammed.hashem@gmail.com>
2023-05-02 04:58:25 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		n++;
+
 		printf("%d\n", h->n);
 		h = h->next;
+		i++;
 	}
 
-	return (n);
+	return (i);
 }
