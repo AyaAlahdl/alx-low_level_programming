e957fed81b4eb7b12db67338e46cb71b851faf9b
AyaAlahdl <aya.mohammed.hashem@gmail.com>
2023-05-02 06:17:40 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,29 +12,31 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp, *copy = *head;
-	unsigned int node;
+	listint_t *node, *p;
+	unsigned int i = 0;
 
-	if (copy == NULL)
+	if (!head || !*head)
 		return (-1);
 
-	if (index == 0)
+	if (!index)
 	{
+		node = *head;
 		*head = (*head)->next;
-		free(copy);
+		free(node);
 		return (1);
 	}
-
-	for (node = 0; node < (index - 1); node++)
+	node = *head;
+	while (node)
 	{
-		if (copy->next == NULL)
-			return (-1);
-
-		copy = copy->next;
+		if (i == index)
+		{
+			p->next = node->next;
+			free(node);
+			return (1);
+		}
+		i++;
+		p = node;
+		node = node->next;
 	}
-
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
-	return (1);
+	return (-1);
 }
